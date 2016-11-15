04e0a723de3ad091a346f92104e5960bce28d583
buharov-alexander <buharovsasha@yandex.ru>
2016-11-15 22:25:56 +0700

    Initial commit. The project was generated by Spring Boot.

---
+++ src/main/java/ru/bukharov/fhelp/FhelpApplication.java
@@ -0,0 +1,12 @@
+package ru.bukharov.fhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhelpApplication.class, args);
+	}
+}
