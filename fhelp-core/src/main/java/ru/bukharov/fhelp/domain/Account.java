9c2d2345d2ea57845ef2f9b328c85934c635b136
buharov-alexander <buharovsasha@yandex.ru>
2017-02-11 10:09:56 +0700

    migrate to posgreSql

--- fhelp-core/src/main/java/ru/bukharov/fhelp/domain/Account.java
+++ fhelp-core/src/main/java/ru/bukharov/fhelp/domain/Account.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,7 +13,7 @@ import javax.persistence.Table;
 public class Account {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer balance;
