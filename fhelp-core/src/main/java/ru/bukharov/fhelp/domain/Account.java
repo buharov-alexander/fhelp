0ebb91bf1982bba3aef3dda412a77f32a6918219
buharov-alexander <buharovsasha@yandex.ru>
2017-04-11 01:08:41 +0700

    add AccountState

--- fhelp-core/src/main/java/ru/bukharov/fhelp/domain/Account.java
+++ fhelp-core/src/main/java/ru/bukharov/fhelp/domain/Account.java
@@ -3,25 +3,32 @@ package ru.bukharov.fhelp.domain;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Entity
 @Table(name = "accounts")
 public class Account {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private Integer balance;
+    private Double balance;
 
     @Enumerated(EnumType.STRING)
     private AccountTypeEnum type;
+
     @Enumerated(EnumType.STRING)
     private ValutaEnum valuta;
+
+    @OneToMany(mappedBy = "accountId", fetch = FetchType.LAZY)
+    private Set<AccountState> states;
 
     public Account() {
     }
@@ -42,14 +49,6 @@ public class Account {
         this.name = name;
     }
 
-    public Integer getBalance() {
-        return balance;
-    }
-
-    public void setBalance(Integer balance) {
-        this.balance = balance;
-    }
-
     public AccountTypeEnum getType() {
         return type;
     }
@@ -58,11 +57,27 @@ public class Account {
         this.type = type;
     }
 
+    public Set<AccountState> getStates() {
+        return states;
+    }
+
+    public void setStates(Set<AccountState> states) {
+        this.states = states;
+    }
+
     public ValutaEnum getValuta() {
         return valuta;
     }
 
     public void setValuta(ValutaEnum valuta) {
         this.valuta = valuta;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
     }
 }
---
+++ fhelp-core/src/main/java/ru/bukharov/fhelp/domain/AccountState.java
@@ -0,0 +1,60 @@
+package ru.bukharov.fhelp.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "accountstates")
+public class AccountState {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "state_date")
+    private Date date;
+    private Double balance;
+
+    @Column(name = "account_id")
+    private Long accountId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}
