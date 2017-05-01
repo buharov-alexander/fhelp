67541c57f4c55ad8bb672f13afe7ed4ffc7fb8a8
buharov-alexander <buharovsasha@yandex.ru>
2017-05-01 16:01:02 +0700

    use rbc/indicators instead of mmvb/rates for account balances

--- fhelp-ui-react/src/main/js/store/store.js
+++ fhelp-ui-react/src/main/js/store/store.js
@@ -13,35 +13,13 @@ function configureStore() {
 
 
 function loadData(store) {
-    const data = {};
-    client({method: 'GET', path: '/fhelp/mmvb/rates'}).then(response => {
-            data.rates = response.entity;
-            return client({method: 'GET', path: '/fhelp/data/accounts'});
-        }).then(response => {
-            const accounts = response.entity._embedded.accounts.map(account =>
-                calculateRubleEquivalent(account, data.rates));
-            data.accounts = accounts;
-            store.dispatch({ type: "LOAD_DATA", payload: data});
-        });
-
     client({method: 'GET', path: '/fhelp/rbc/indicators'}).then(response => {
         const indicators = response.entity;
         store.dispatch({ type: "LOAD_INDICATORS", payload: indicators});
+        return client({method: 'GET', path: '/fhelp/data/accounts'});
+    }).then(response => {
+        store.dispatch({ type: "LOAD_ACCOUNTS", payload: response.entity._embedded.accounts});
     });
-}
-
-function calculateRubleEquivalent(account, rates) {
-    switch(account.valuta) {
-        case 'USD':
-            account.rubBalance = Math.round(account.balance*rates.USD*10)/10;
-            break;
-        case 'EUR':
-            account.rubBalance = Math.round(account.balance*rates.EUR*10)/10;
-            break;
-        default:
-            account.rubBalance = account.balance;
-    }
-    return account;
 }
 
 export {configureStore, loadData};
