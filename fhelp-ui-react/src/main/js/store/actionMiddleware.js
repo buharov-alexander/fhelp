cca1e698622a9f8072d0bd235d2469e017d5ef54
buharov-alexander <buharovsasha@yandex.ru>
2017-05-28 19:19:13 +0700

    remove host name from code

--- fhelp-ui-react/src/main/js/store/actionMiddleware.js
+++ fhelp-ui-react/src/main/js/store/actionMiddleware.js
@@ -7,7 +7,7 @@ export default store => next => action => {
     if (action.type === 'ADD_ACCOUNT') {
         const newAccount = action.payload;
         client({method: 'POST',
-            path: 'http://localhost:8080/fhelp/account', 
+            path: '/fhelp/account', 
             entity: newAccount, 
             headers: {'Content-Type': 'application/json'}}).then(response => {
                 newAccount.id = response.entity.id;
@@ -15,13 +15,13 @@ export default store => next => action => {
             });
     } else if (action.type === 'DELETE_ACCOUNT') {
         client({method: 'DELETE',
-            path: 'http://localhost:8080/fhelp/data/accounts/' + action.payload}).then(response => {
+            path: '/fhelp/data/accounts/' + action.payload}).then(response => {
                 store.dispatch({type: 'DELETE_ACCOUNT_SUCCESS', payload: action.payload});
             }); 
     } else if (action.type === 'ADD_ACCOUNT_STATE') {
         const state = action.payload;
         client({method: 'POST',
-            path: 'http://localhost:8080/fhelp/account/state', 
+            path: '/fhelp/account/state', 
             entity: state, 
             headers: {'Content-Type': 'application/json'}}).then(response => {
                 store.dispatch({type: 'ADD_ACCOUNT_STATE_SUCCESS', payload: response.entity});
