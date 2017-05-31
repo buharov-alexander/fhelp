b378a93d69debc9a8e99885785344ba5c000dcdf
buharov-alexander <buharovsasha@yandex.ru>
2017-05-31 23:09:46 +0700

    disable csrf

--- fhelp-core/src/main/java/ru/bukharov/fhelp/WebSecurityConfig.java
+++ fhelp-core/src/main/java/ru/bukharov/fhelp/WebSecurityConfig.java
@@ -13,8 +13,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/fhelp/rbc/*", "/fhelp/mmvb/*", "/fhelp/rambler/*").permitAll()
+                .antMatchers("/fhelp/rbc/*", "/fhelp/mmvb/*", "/fhelp/rambler/*",
+                        "/built/bundle.js", "/*.css").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
